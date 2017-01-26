d03dbe568f2be71064604e370202a3bd1c42d411
David Morales <David_Morales@epam.com>
2017-01-25 18:08:36 -0600

    Apply JS standard rules

--- app/index.jsx
+++ app/index.jsx
@@ -1,57 +1,57 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Rebase from 're-base'
-const disciplines = 'disciplines';
+const disciplines = 'disciplines'
 const base = Rebase.createClass({
   apiKey: '',
   authDomain: '',
   databaseURL: 'DATABASE_URL',
   storageBucket: ''
-});
+})
 
 class Home extends React.Component {
-  constructor() {
+  constructor () {
     super()
     this.state = {
       disciplines: []
     }
   }
-  handleSubmit(e) {
-    var newDiscipline = this.discipline.value;
-    this.discipline.value = '';
+  handleSubmit (e) {
+    var newDiscipline = this.discipline.value
+    this.discipline.value = ''
     base.post(disciplines, {
       data: this.state.disciplines.concat([newDiscipline])
     })
   }
-  setRef(ref) {
+  setRef (ref) {
     this.discipline = ref
   }
-  componentDidMount() {
+  componentDidMount () {
     this.ref = base.bindToState(disciplines, {
       context: this,
       asArray: true,
       state: 'disciplines'
     })
   }
-  componentWillUnmount() {
+  componentWillUnmount () {
     base.removeBinding(this.ref)
   }
-  render() {
+  render () {
     return (
-      <div className='col-sm-12'>
+      <div className="col-sm-12">
         <h1>Hello Firebase</h1>
-        <form className='form-inline'>
-          <div className='form-group'>
-             <input type='text' className='form-control'
-               placeholder='Discipline'
-               ref={(ref) => this.setRef(ref)} />
+        <form className="form-inline">
+          <div className="form-group">
+            <input type="text" className="form-control"
+              placeholder="Discipline"
+              ref={(ref) => this.setRef(ref)} />
           </div>
-          <button type='button'
-              className='btn btn-default'
-              onClick={(e) => this.handleSubmit(e)}>Add new</button>
+          <button type="button"
+            className="btn btn-default"
+            onClick={(e) => this.handleSubmit(e)}>Add new</button>
         </form>
 
-        <ul className='row'>
+        <ul className="row">
           {this.state.disciplines.map((discipline, i) => (
             <li key={i}>{discipline}</li>
           ))}
